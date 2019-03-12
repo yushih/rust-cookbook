bde1cbf3182c8a9c747e2970f17391b70e19c1a9
yushi <yushihuang@gmail.com>
2019-03-12 14:55:41 +0800

    refactor: use ref count to avoid cloning

--- 1.3. Keeping the Last N Items/src/main.rs
+++ 1.3. Keeping the Last N Items/src/main.rs
@@ -1,34 +1,36 @@
+use std::rc::Rc;
 use std::io::{BufRead, BufReader, Result};
 use std::fs::File;
 use std::collections::VecDeque;
 
-struct HistoryIterator<I: Iterator> where I::Item: Clone {
-    pub history: VecDeque<I::Item>,
+struct HistoryIterator<I: Iterator>  {
+    pub history: VecDeque<Rc<I::Item>>,
     length: usize,
     iterator: I
 }
 
-impl<I: Iterator> HistoryIterator<I> where I::Item: Clone {
+impl<I: Iterator> HistoryIterator<I>  {
     pub fn new(iterator: I, length: usize) -> HistoryIterator<I> {
         HistoryIterator { history: VecDeque::new(), length, iterator}
     }
 
-    pub fn get_history(&self) -> std::iter::Take<std::collections::vec_deque::Iter<I::Item>> {
+    pub fn get_history(&self) -> std::iter::Take<std::collections::vec_deque::Iter<Rc<I::Item>>> {
         self.history.iter().take(self.history.len()-1)
     }
 }
 
-impl<I: Iterator> Iterator for HistoryIterator<I> where I::Item: Clone {
-    type Item = I::Item;
+impl<I: Iterator> Iterator for HistoryIterator<I> {
+    type Item = Rc<I::Item>;
     fn next(&mut self) -> Option<Self::Item> {
         match self.iterator.next() {
             Some(i) => {
-                self.history.push_back(i.clone());
+                let r = Rc::new(i);
+                self.history.push_back(r.clone());
                 // must keep one more before the last item is the current item and not returned by get_history()
                 if self.history.len() > self.length +1 {
                     self.history.pop_front();
                 }
-                return Some(i);
+                return Some(r);
             },
             None => { None }
         }
